299fe6f9acaef307c91e719c8ef1cf7e33976fe3
Hotaka Makiuchi <s.hotaka.makiuchi@iridge.jp>
2025-09-07 18:58:01 +0900

    Initial commit: API接続アプリ
    - API接続機能のみのシンプルなFlutterアプリ
    - サンプルAPIからデータを取得・表示
    - 検索・フィルタリング・ソート機能
    - 不要な依存関係を削除してクリーンアップ

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'screens/api_connection_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'API接続',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ApiConnectionScreen(),
+    );
+  }
+}
